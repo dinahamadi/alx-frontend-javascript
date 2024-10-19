7e0e72df4901e473f01fdfa1b9d6de158c87f882
dina <dinahamadi.20@gmail.com>
2024-10-19 04:49:01 +0100

    fixing 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    const task = trueOrFalse;
-    const task2 = !trueOrFalse;
+  let task = false;
+  let task2 = true;
 
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
   return [task, task2];
 }
