094e4b0b8b4728c490dca6c27afec47836cab9f2
dina <dinahamadi.20@gmail.com>
2024-10-26 03:49:29 +0100

    5

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,13 +1,9 @@
 import Building from './5-building.js';
 
-const b = new Building(100);
-console.log(b);
-
 class TestBuilding extends Building { }
 
-try {
-    new TestBuilding(200)
-}
-catch (err) {
-    console.log(err);
-}
+test("Building forces override", () => {
+    expect(() => {
+        new TestBuilding(200);
+    }).toThrowError("Class extending Building must override evacuationWarningMessage");
+});
