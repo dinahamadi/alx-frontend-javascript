099a42b5de0ac956cdcfda4ce8e532c2fdcb8418
dina <dinahamadi.20@gmail.com>
2024-10-26 03:39:16 +0100

    fixing 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-underscore-dangle */
-
 export default class Building {
   constructor(sqft) {
     if (this.constructor === Building) {
@@ -12,15 +11,6 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
-
-  // Setter for sqft (optional, depending on your needs)
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Square footage must be a number');
-    }
-    this._sqft = value;
-  }
-
   // Abstract method for subclasses to implement
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
