58e84320783fc758aadbac05642093c844b165e5
dina <dinahamadi.20@gmail.com>
2024-10-26 03:45:41 +0100

    fixing 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
-  constructor(sqft) {
+  constructor(sqft = 0) {
     if (this.constructor === !Building
         && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   // Getter for sqft
