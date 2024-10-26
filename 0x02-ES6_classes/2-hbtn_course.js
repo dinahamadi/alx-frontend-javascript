d4a73380fdb1eb375d88a8e9fa62aa7cece6b339
dina <dinahamadi.20@gmail.com>
2024-10-26 03:10:23 +0100

    fixing 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -12,7 +12,9 @@ export default class HolbertonCourse {
 
   // Setter for name with validation
   set name(value) {
-    this._validateString(value);
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
 
@@ -21,9 +23,11 @@ export default class HolbertonCourse {
     return this._length;
   }
 
-  // Setter for length with validation
+  // Setter for length
   set length(value) {
-    this._validateNumber(value);
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
 
@@ -34,29 +38,10 @@ export default class HolbertonCourse {
 
   // Setter for students with validation
   set students(value) {
-    this._validateArray(value);
-    this._students = value;
-  }
-
-  // Private method to validate string
-  _validateString(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  // Private method to validate number
-  _validateNumber(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-  }
-
-  // Private method to validate array
-  _validateArray(value) {
     if (!Array.isArray(value)
-            || !value.every((item) => typeof item === 'string')) {
+              || !value.every((item) => typeof item === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
+    this._students = value;
   }
 }
