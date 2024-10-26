3574c785f7ee6280cff4960bfc6902525b3c0538
dina <dinahamadi.20@gmail.com>
2024-10-26 03:29:46 +0100

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error('Cannot instantiate abstract class Building');
+    }
+    this.sqft = sqft;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Setter for sqft (optional, depending on your needs)
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+    this._sqft = value;
+  }
+
+  // Abstract method for subclasses to implement
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
